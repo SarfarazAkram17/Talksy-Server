8c2af34c5e77c7f802b2766f39f247d6ab94d6ff
sarfarazakram576 <sarfaraz.akram055@gmail.com>
2025-08-14 01:04:10 +0600

    update client side url

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://talksy-sarfaraz-akram.netlify.app"],
     credentials: true,
   })
 );
